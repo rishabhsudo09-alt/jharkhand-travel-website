e93d2acf4703c3d719e90ddb9219f1153d8c46ec
Rishabh <108469021+Rishabh-raj-kumar@users.noreply.github.com>
2025-09-28 20:24:18 +0530

    added image features

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,9 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.rules(
+    { "react/no-unescaped-entities": 0 }
+  ),
   {
     ignores: [
       "node_modules/**",
